3bae1fdd57eceb8a5b198cc18ee14299ad178c17
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2021-01-10 13:17:54 +0500

    implemented the entire functionality of shopping cart; just need to add more products

--- src/components/ProductReducer.js
+++ src/components/ProductReducer.js
@@ -1,20 +1,55 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
 import shoes from "../shoes.json";
 
 const productReducer = createSlice({
   name: "productReducer",
-  initialState: [],
+  initialState: {
+    shoeArray: [],
+    itemsLength: 0,
+    totalPrice: 0,
+  },
   reducers: {
     addProduct: (state, action) => {
       const { keyName } = action.payload;
+      if (state.itemsLength > 0) {
+        for (let i = 0; i < state.shoeArray.length; i++) {
+          if (state.shoeArray[i].id === keyName) {
+            console.log(state.shoeArray[i].quantity);
+            state.shoeArray[i].quantity += 1;
+            break;
+          } else if (i === state.shoeArray.length - 1) {
+            state.shoeArray.push(shoes[keyName]);
+            break;
+          }
+        }
+        state.totalPrice += Number(shoes[keyName].price.substring(1));
+      } else {
+        state.shoeArray.push(shoes[keyName]);
+        state.totalPrice += Number(state.shoeArray[0].price.substring(1));
+      }
+      state.itemsLength += 1;
       console.log(keyName);
-      state.push(shoes[keyName]);
-      console.log(state);
     },
-    removeProduct: (state, action) => {},
+    removeProduct: (state, action) => {
+      const { keyName } = action.payload;
+      state.shoeArray.forEach((shoe, ind) => {
+        if (shoe.id === keyName) {
+          if (shoe.quantity > 1) {
+            shoe.quantity -= 1;
+          } else {
+            state.shoeArray.splice(ind, 1);
+          }
+          state.totalPrice -= Number(shoe.price.substring(1));
+          state.itemsLength -= 1;
+        }
+      });
+    },
   },
 });
-export const shoppingCartArray = (state) => state;
+export const shoppingCartArray = (state) => ({
+  shoeArray: state.shoeArray,
+  itemsLength: state.itemsLength,
+  totalPrice: state.totalPrice,
+});
 export const { addProduct, removeProduct } = productReducer.actions;
 export default productReducer.reducer;
